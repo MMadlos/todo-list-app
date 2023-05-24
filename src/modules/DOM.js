f29aeec25e5b48f53a09c900d5574e07ab9e24a3
michael <michaelmadlos@hotmail.com>
2023-05-24 11:21:30 +0200

    Delete old code

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,21 +1,6 @@
-import IconNotChecked from "../icons/not-checked.svg"
-import IconCheckedDefault from "../icons/Checked-selected.svg"
-import IconCheckedBorderBlue from "../icons/Checked-border-highlighted.svg"
-import IconCheckedNotSelected from "../icons/Checked-not-selected.svg"
-import IconPriority from "../icons/priority.svg"
-import IconCalendar from "../icons/calendar.svg"
-import IconFolder from "../icons/folder.svg"
-import IconChevronRight from "../icons/chevron-right.svg"
-import IconChevronDown from "../icons/chevron-down.svg"
-import IconChevronLeft from "../icons/chevron-left.svg"
-import IconAttach from "../icons/attach.svg"
-import IconAdd from "../icons/add.svg"
-
-import tickIcon, { Label, Logo, IconGenerator } from "./icons"
+import { Logo, IconGenerator } from "./icons"
 
 const content = document.getElementById("content")
-
-//!! NEW UI
 
 function el(element) {
 	return document.createElement(element)
@@ -26,9 +11,7 @@ export function newUI() {
 	const taskPanel = taskPanelComponent()
 	const taskList = mainSectionComponent()
 
-	content.appendChild(menu)
-	content.appendChild(taskList)
-	content.appendChild(taskPanel)
+	content.append(menu, taskList, taskPanel)
 }
 
 function menuComponent() {
@@ -49,26 +32,34 @@ function menuComponent() {
 
 	menu.appendChild(menuContainer)
 	menuContainer.appendChild(tituloContainer)
-	tituloContainer.appendChild(logo)
-	tituloContainer.appendChild(titulo)
+	tituloContainer.append(logo, titulo)
 
 	//PROJECT LIST
 	//-> Default list
 	const projectListDefault = el("div")
 	projectListDefault.className = "project-list-container"
 
-	const projectPlanificado = projectItem("clock", "Planificado")
-	const projectTodos = projectItem("list", "Todos")
-	const projectImportantes = projectItem("star", "Importantes")
-	const projectCompletados = projectItem("check", "Completados")
+	const projectsDefault = {
+		planificado: ["clock", "Planificado"],
+		todos: ["list", "Todos"],
+		importantes: ["star", "Importantes"],
+		completados: ["check", "Completados"],
+	}
 
-	projectPlanificado.classList.add("selected")
+	const projectsLength = Object.keys(projectsDefault).length
+	for (var i = 0; i < projectsLength; i++) {
+		let projectIconName = Object.values(projectsDefault)[i][0]
+		let projectText = Object.values(projectsDefault)[i][1]
+
+		let project = projectItem(projectIconName, projectText)
+		if (i === 0) {
+			project.classList.add("selected")
+		}
+
+		projectListDefault.appendChild(project)
+	}
 
 	menuContainer.appendChild(projectListDefault)
-	projectListDefault.appendChild(projectPlanificado)
-	projectListDefault.appendChild(projectTodos)
-	projectListDefault.appendChild(projectImportantes)
-	projectListDefault.appendChild(projectCompletados)
 
 	//-> Personalized list
 	const projectList = el("div")
@@ -388,7 +379,7 @@ function taskCardUI() {
 	taskInfoContainer.className = "task-info-container"
 
 	// -> Tick Icon
-	const tickIcon = IconGenerator("check", "size-21") //TODO Sustituir
+	const tickIcon = IconGenerator("checkEmpty", "size-21") //TODO Sustituir
 
 	// -> Task title and info
 	const taskTitleContainer = el("div")
@@ -443,425 +434,4 @@ function taskCardUI() {
 	taskCardContainer.append(taskInfoContainer, iconStar)
 
 	return taskCardContainer
-}
-
-//! OLD UI ----------->
-
-// Elements for components
-
-export function Button() {
-	const btn = document.createElement("button")
-	btn.className = "btn"
-
-	function addTask() {
-		btn.textContent = "Nueva tarea"
-		btn.classList.add("call-to-action")
-		btn.id = "addTask"
-		return btn
-	}
-
-	function saveTask() {
-		btn.textContent = "Guardar"
-		btn.classList.add("call-to-action")
-		btn.id = "saveTask"
-
-		return btn
-	}
-
-	function dropDown(text) {
-		btn.textContent = text
-		btn.classList.add("drop-Down")
-
-		const iconDropDown = icon("chevronDown")
-
-		btn.appendChild(iconDropDown)
-
-		return btn
-	}
-
-	function moreDetailsTask() {
-		const moreDetailsBtn = document.createElement("div")
-		const moreDetailsIcon = icon("chevronRight")
-
-		moreDetailsBtn.className = "more-details-btn-container"
-		moreDetailsIcon.classList.add("more-details-btn")
-
-		moreDetailsBtn.appendChild(moreDetailsIcon)
-
-		return moreDetailsBtn
-	}
-
-	function deleteTask() {
-		btn.textContent = "Eliminar"
-		btn.classList.add("delete")
-		btn.id = "delete"
-
-		return btn
-	}
-
-	return { btn, addTask, saveTask, dropDown, moreDetailsTask, deleteTask }
-}
-
-export function icon(type) {
-	const icon = new Image()
-
-	if (type === "logo") icon.src = LogoTasks
-
-	if (type === "notChecked") icon.src = IconNotChecked
-	if (type === "checkedDefault") icon.src = IconCheckedDefault
-	if (type === "checkedBorderBlue") icon.src = IconCheckedBorderBlue
-	if (type === "checkedNotSelected") icon.src = IconCheckedNotSelected
-
-	if (type === "chevronRight") icon.src = IconChevronRight
-	if (type === "chevronDown") icon.src = IconChevronDown
-	if (type === "chevronLeft") icon.src = IconChevronLeft
-
-	if (type === "attach") icon.src = IconAttach
-	if (type === "add") icon.src = IconAdd
-
-	return icon
-}
-
-// export function SVG() {
-// 	const add = (svgFile) => {
-// 		const svgObject = document.createElement("object")
-// 		svgObject.data = svgFile
-// 		svgObject.type = "image/svg+xml"
-// 		svgObject.className = "label-icon"
-
-// 		return svgObject
-// 	}
-// 	const setColor = (color) => {
-// 		window.addEventListener("load", () => {
-// 			const objectElement = objectContainer.querySelector("object")
-// 			const svg = objectElement.contentDocument.querySelector("svg")
-// 			const path = svg.querySelectorAll("path")
-
-// 			path.forEach((element) => {
-// 				element.setAttribute("fill", color)
-// 			})
-// 		})
-// 	}
-
-// 	return { add, setColor }
-// }
-
-export function setSVGColor(objectContainer, color) {
-	window.addEventListener("load", () => {
-		const objectElement = objectContainer.querySelector("object")
-		const svg = objectElement.contentDocument.querySelector("svg")
-		const path = svg.querySelectorAll("path")
-
-		path.forEach((element) => {
-			element.setAttribute("fill", color)
-		})
-	})
-}
-
-function label(iconType) {
-	const label = document.createElement("div")
-	label.className = "label"
-	label.classList.add(iconType)
-
-	const text = document.createElement("p")
-	text.textContent = "Sin prioridad"
-	text.className = "label-text"
-
-	// Icon label
-	let svg
-
-	if (iconType === "priority") svg = SVG().add(IconPriority)
-	if (iconType === "calendar") svg = SVG().add(IconCalendar)
-	if (iconType === "folder") svg = SVG().add(IconFolder)
-
-	label.appendChild(svg)
-	label.appendChild(text)
-
-	return label
-}
-
-function separatorLine() {
-	const separatorLine = document.createElement("div")
-	separatorLine.className = "separator-line"
-
-	return separatorLine
-}
-
-// Components
-function header() {
-	const headerContainer = document.createElement("div")
-	headerContainer.className = "header-container"
-
-	const header = document.createElement("h1")
-	header.textContent = "Mis tareas"
-	header.className = "header"
-
-	const logo = icon("logo")
-	logo.className = "logo"
-
-	headerContainer.appendChild(logo)
-	headerContainer.appendChild(header)
-
-	return headerContainer
-}
-
-function buttonsSection() {
-	const sectionButtons = document.createElement("div")
-	sectionButtons.className = "buttons-section"
-
-	const dropDownContainer = document.createElement("div")
-	dropDownContainer.className = "dropDown-container"
-
-	sectionButtons.appendChild(dropDownContainer)
-	dropDownContainer.appendChild(Button().dropDown("Mostrar todo"))
-	dropDownContainer.appendChild(Button().dropDown("Todos los proyectos"))
-
-	sectionButtons.appendChild(Button().addTask())
-
-	return sectionButtons
-}
-
-function taskAccordion() {
-	const taskAccordionContainer = document.createElement("div")
-	taskAccordionContainer.className = "task-accordion-container"
-
-	const separatorInfoContainer = document.createElement("div")
-	separatorInfoContainer.className = "task-accordion-info-container"
-
-	const dropDownIcon = icon("chevronDown")
-	dropDownIcon.className = "drop-down"
-
-	const text = document.createElement("p")
-	text.textContent = "Sin fecha"
-	text.className = "separator-text"
-
-	const counterContainer = document.createElement("div")
-	counterContainer.className = "counter-container"
-
-	const counterText = document.createElement("p")
-	counterText.textContent = "8" //TODO <<----- CAMBIAR
-	counterText.className = "counter-text"
-
-	taskAccordionContainer.appendChild(separatorInfoContainer)
-	separatorInfoContainer.appendChild(dropDownIcon)
-	separatorInfoContainer.appendChild(text)
-	separatorInfoContainer.appendChild(counterContainer)
-	counterContainer.appendChild(counterText)
-	taskAccordionContainer.appendChild(separatorLine())
-
-	return taskAccordionContainer
-}
-
-export function TaskComponent(title, taskStatus) {
-	// Template
-	const taskContainer = document.createElement("div")
-	taskContainer.className = "task-container"
-
-	const taskDataContainer = document.createElement("div")
-	taskDataContainer.className = "task-data-container"
-
-	const taskDetailsContainer = document.createElement("div")
-	taskDetailsContainer.className = "task-details-container"
-
-	const taskText = document.createElement("p")
-	taskText.textContent = title
-	taskText.className = "task-text"
-
-	const taskLabelsContainer = document.createElement("div")
-	taskLabelsContainer.className = "labels-container"
-
-	const iconContainer = document.createElement("div")
-	iconContainer.className = "icon-container"
-	const taskIcon = tickIcon("default")
-	const moreOptionButton = Button().moreDetailsTask()
-
-	taskContainer.appendChild(taskDataContainer)
-	taskContainer.appendChild(moreOptionButton)
-
-	taskDataContainer.appendChild(iconContainer)
-	iconContainer.appendChild(taskIcon)
-	taskDataContainer.appendChild(taskDetailsContainer)
-	taskDetailsContainer.appendChild(taskText)
-	taskDetailsContainer.appendChild(taskLabelsContainer)
-
-	// Labels
-	taskLabelsContainer.appendChild(Label("hasNotPriority", taskStatus))
-	taskLabelsContainer.appendChild(Label("calendar", taskStatus))
-	taskLabelsContainer.appendChild(Label("folder", taskStatus))
-
-	return taskContainer
-}
-
-export function ReturnComponent() {
-	const returnComponent = document.createElement("div")
-	returnComponent.className = "return-component"
-
-	const returnIcon = SVG(IconChevronLeft)
-	returnIcon.className = "return-icon"
-
-	const returnText = document.createElement("p")
-	returnText.className = "return-text"
-	returnText.textContent = "Volver"
-
-	returnComponent.appendChild(returnIcon)
-	returnComponent.appendChild(returnText)
-
-	return returnComponent
-}
-
-export function TaskForm() {
-	const taskFormContainer = document.createElement("div")
-	taskFormContainer.className = "task-form-container"
-
-	// TASK DETAILS SECTION
-	const infoContainer = document.createElement("div")
-	infoContainer.className = "info-container"
-
-	// -> Title and subtasks component
-	const titleTaskComponent = document.createElement("div")
-	titleTaskComponent.className = "title-task-component"
-
-	// --> Title
-	const titleContainer = document.createElement("div")
-	titleContainer.className = "title-container"
-
-	const tickIcon = icon("notChecked")
-	tickIcon.dataset.type = "isCompleted"
-	tickIcon.dataset.state = "false"
-
-	const taskTitleLabel = document.createElement("label")
-	taskTitleLabel.htmlFor = "task-title"
-
-	const taskTitle = document.createElement("input")
-	taskTitle.value = "Título añadido"
-	taskTitle.type = "text"
-	taskTitle.id = "task-title"
-	taskTitle.placeholder = "Escribe el título de la tarea"
-
-	// --> Subtasks
-	const subtasksContainer = document.createElement("div")
-	subtasksContainer.className = "subtasks-container"
-
-	const subtaskContent = document.createElement("div")
-	subtaskContent.className = "subtask-content"
-
-	const subtaskIcon = icon("add")
-	const subtaskText = document.createElement("p")
-	subtaskText.textContent = "Agregar paso"
-
-	titleTaskComponent.appendChild(titleContainer)
-	titleContainer.appendChild(tickIcon)
-	titleContainer.appendChild(taskTitleLabel)
-	titleContainer.appendChild(taskTitle)
-
-	titleTaskComponent.appendChild(subtasksContainer)
-	subtasksContainer.appendChild(subtaskContent)
-	subtaskContent.appendChild(subtaskIcon)
-	subtaskContent.appendChild(subtaskText)
-
-	infoContainer.appendChild(titleTaskComponent)
-	infoContainer.appendChild(separatorLine())
-
-	// -> Task info details component
-	const taskInfoDetailsContainer = document.createElement("div")
-	taskInfoDetailsContainer.className = "task-info-details-container"
-
-	function infoDetailsComponent(type) {
-		const container = document.createElement("div")
-		container.className = "info-detail-component-container"
-
-		// const iconDetail = svg(type)
-		const textDetail = document.createElement("p")
-
-		let svg
-		if (type === "priority") {
-			svg = SVG(IconPriority, "form-icon")
-			textDetail.textContent = "Marcar como prioritario"
-		}
-		if (type === "calendar") {
-			svg = SVG(IconCalendar, "form-icon")
-			textDetail.textContent = "Agregar fecha de vencimiento"
-		}
-		if (type === "folder") {
-			svg = SVG(IconFolder, "form-icon")
-			textDetail.textContent = "Asignar proyecto"
-		}
-
-		svg.dataset.type = type
-		svg.dataset.state = "false"
-
-		container.appendChild(svg)
-		container.appendChild(textDetail)
-
-		return container
-	}
-
-	taskInfoDetailsContainer.appendChild(infoDetailsComponent("priority"))
-	taskInfoDetailsContainer.appendChild(separatorLine())
-	taskInfoDetailsContainer.appendChild(infoDetailsComponent("calendar"))
-	taskInfoDetailsContainer.appendChild(separatorLine())
-	taskInfoDetailsContainer.appendChild(infoDetailsComponent("folder"))
-
-	infoContainer.appendChild(taskInfoDetailsContainer)
-
-	// -> Attach file component
-	const attachContainer = document.createElement("div")
-	attachContainer.className = "task-info-details-container"
-
-	const infoAttachContainer = document.createElement("div")
-	infoAttachContainer.className = "info-detail-component-container"
-
-	const attachText = document.createElement("p")
-	attachText.textContent = "Adjuntar archivo"
-
-	const attachIcon = SVG(IconAttach, "form-icon")
-
-	attachContainer.appendChild(infoAttachContainer)
-	infoAttachContainer.appendChild(attachIcon)
-	infoAttachContainer.appendChild(attachText)
-
-	infoContainer.appendChild(attachContainer)
-
-	// -> Add note section
-	const addNote = document.createElement("textarea")
-	addNote.name = "add-note"
-	addNote.rows = "5"
-	addNote.id = "addNote"
-	addNote.placeholder = "Agregar nota"
-
-	infoContainer.appendChild(addNote)
-
-	// BUTTONS SECTION
-	const buttonsContainer = document.createElement("div")
-	buttonsContainer.className = "buttons-container"
-
-	const saveButton = Button().saveTask()
-
-	const deleteButton = Button().deleteTask()
-
-	taskFormContainer.appendChild(infoContainer)
-
-	taskFormContainer.appendChild(buttonsContainer)
-	buttonsContainer.appendChild(saveButton)
-	buttonsContainer.appendChild(deleteButton)
-
-	return taskFormContainer
-}
-
-// Page Layout (Default)
-export function UI() {
-	const section = document.createElement("section")
-	section.id = "section"
-
-	content.appendChild(section)
-	section.appendChild(header())
-	section.appendChild(buttonsSection())
-
-	const taskListContainer = document.createElement("div")
-	taskListContainer.className = "task-list-container"
-
-	section.appendChild(taskListContainer)
-	taskListContainer.appendChild(taskAccordion())
-
-	return content
 }
--- src/modules/icons.js
+++ src/modules/icons.js
@@ -1,16 +1,4 @@
 import LogoTasks from "../icons/tasks.png"
-import IconNotChecked from "../icons/not-checked.svg"
-import IconCheckedDefault from "../icons/Checked-selected.svg"
-import IconCheckedBorderBlue from "../icons/Checked-border-highlighted.svg"
-import IconCheckedNotSelected from "../icons/Checked-not-selected.svg"
-import IconPriority from "../icons/priority.svg"
-import IconCalendar from "../icons/calendar.svg"
-import IconFolder from "../icons/folder.svg"
-import IconChevronRight from "../icons/chevron-right.svg"
-import IconChevronDown from "../icons/chevron-down.svg"
-import IconChevronLeft from "../icons/chevron-left.svg"
-import IconAttach from "../icons/attach.svg"
-import IconAdd from "../icons/add.svg"
 
 export function Logo() {
 	const _logo = new Image()
@@ -30,7 +18,6 @@ export function IconGenerator(iconName = "bookmark", size) {
 		add: ["fa-solid", "fa-plus"],
 		list: ["fa-solid", "fa-list"],
 		star: ["fa-regular", "fa-star"],
-		check: ["fa-regular", "fa-square-check"],
 		play: ["fa-regular", "fa-circle-play"],
 		bookmark: ["fa-solid", "fa-bookmark"],
 		close: ["fa-solid", "fa-xmark"],
@@ -38,87 +25,12 @@ export function IconGenerator(iconName = "bookmark", size) {
 		clip: ["fa-solid", "fa-paperclip"],
 		chevronRight: ["fa-solid", "fa-chevron-right"],
 		chevronDown: ["fa-solid", "fa-chevron-down"],
+		check: ["fa-regular", "fa-square-check"],
+		checkEmpty: ["fa-regular", "fa-square"],
+		checkDone: ["fa-solid", "fa-square-check"],
 	}
 
 	const _icon = document.createElement("i")
 	_icon.classList.add(..._classes[iconName], size)
 	return _icon
-}
-
-// Tick icon
-export default function tickIcon(state) {
-	const properties = {
-		default: IconNotChecked,
-		defaultHover: IconCheckedDefault,
-		completedDefault: IconCheckedNotSelected,
-		completedHover: IconCheckedBorderBlue,
-	}
-
-	const icon = SVG(properties[state])
-	icon.classList.add("task-icon")
-
-	return icon
-}
-
-// Label icons
-function labelIcon(iconType, taskStatus) {
-	const svgProperties = {
-		hasNotPriority: IconPriority,
-		hasPriority: IconPriority,
-		calendar: IconCalendar,
-		folder: IconFolder,
-	}
-
-	const svg = SVG(svgProperties[iconType])
-
-	window.addEventListener("load", () => {
-		const path = svg.contentDocument.querySelectorAll("path")
-
-		if (taskStatus === "notCompleted" && iconType === "hasPriority") {
-			path.forEach((element) => {
-				element.setAttribute("fill", "#c")
-			})
-		}
-
-		if (taskStatus === "completed" && iconType !== "calendar") {
-			path.forEach((element) => {
-				element.setAttribute("fill", "#c6ddec")
-			})
-		}
-
-		if (taskStatus === "completed" && iconType === "calendar") {
-			path.forEach((element) => {
-				element.setAttribute("stroke", "#c6ddec")
-				element.setAttribute("fill", "#fcfcfc")
-			})
-		}
-	})
-
-	return svg
-}
-
-export function Label(iconType, taskStatus) {
-	const label = document.createElement("div")
-	label.className = "label"
-	label.classList.add(iconType)
-
-	const text = document.createElement("p")
-	text.textContent = "Default Text"
-	text.className = "label-text"
-
-	const svg = labelIcon(iconType, taskStatus)
-	if (iconType === "hasPriority") {
-		label.classList.add("has-priority")
-	}
-
-	if (taskStatus === "completed") {
-		label.classList.add("completed")
-	}
-
-	label.appendChild(svg)
-	label.appendChild(text)
-
-	//TODO --> Falta si el label es de una tarea completada (azul claro)
-
-	return label
 }
